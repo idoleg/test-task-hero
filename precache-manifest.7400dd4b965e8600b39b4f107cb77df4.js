4720eaddd7fa89e7ace1a0348c9dcaea397542c6
idoleg <idoleg@users.noreply.github.com>
2019-12-19 13:36:36 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.7400dd4b965e8600b39b4f107cb77df4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16ca3dfdf16c2611848280e34d7ac24",
+    "url": "/test-task-hero/index.html"
+  },
+  {
+    "revision": "a81a7e7c12bb2dd9e366",
+    "url": "/test-task-hero/static/js/2.dee23df6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test-task-hero/static/js/2.dee23df6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f734233fb7a2e5448e3",
+    "url": "/test-task-hero/static/js/main.b134ec8f.chunk.js"
+  },
+  {
+    "revision": "15e595f29ce83f304d78",
+    "url": "/test-task-hero/static/js/runtime-main.f65b2fc8.js"
+  }
+]);
